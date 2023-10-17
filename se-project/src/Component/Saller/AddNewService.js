d775fc2f643fd35a31ebfaab4570a5f4fab9fc30
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-10-16 22:11:28 -0700

    working

--- se-project/src/Component/Saller/AddNewService.js
+++ se-project/src/Component/Saller/AddNewService.js
@@ -5,6 +5,9 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { useState, useRef } from 'react';
 import Discrestion from './Discrestion';
+import ReactLoading from "react-loading";
+import {DescriptionData} from './Discrestion.js'
+import Gallary from './Gallary.js'
 
 
 const services = [
@@ -33,16 +36,26 @@ const services = [
 
 const AddNewService = () => {
     const [valid, setValid] = React.useState(false);
+    const [loading, setLoading] = useState(false);
     const [selectedService, setSelectedService] = useState('');
     const [selectedTasks, setSelectedTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
     const [showInput, setShowInput] = useState(true);
     const [showDiscrestion, setShowDiscrestion] = useState(false);
+    const [Gallary_, setGallary] = useState(false);
+   
+    const UpDateShow = () => {
+        setShowDiscrestion(false);
+        setGallary(true);
+    }
     const [formData, setFormData] = useState({
         ServiceTitle: '',
-        lastName: '',
         BasicPrice: '',
-        message: '',
+        Tags: [],
+        ServiceType: '',
+        Description: DescriptionData,
+
+
     });
     const handleChangeInput = (e) => {
         const { name, value } = e.target;
@@ -52,8 +65,13 @@ const AddNewService = () => {
         });
     };
 
-    const handleChange = (event) => {
-        setSelectedService(event.target.value);
+    const handleChange = (e) => {
+        setSelectedService(e.target.value);
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
     };
     const handleTaskChange = (event) => {
         setSelectedTasks(event.target.value);
@@ -63,12 +81,44 @@ const AddNewService = () => {
     };
     const handleTaskAdd = () => {
         if (newTask && selectedTasks.length < 5) {
+            setFormData({
+                ...formData,
+                Tags: [...selectedTasks, newTask],
+            });
             setSelectedTasks([...selectedTasks, newTask]);
             setNewTask('');
         }
     };
 
-    const SaveData = () => {
+    const SaveData = (event) => {
+        event.preventDefault();
+        if (formData.ServiceTitle.length === 0 || formData.BasicPrice.length === 0 || formData.ServiceType.length === 0 || formData.Tags.length === 0) {
+            alert('Please Fill All The Fields');
+            return;
+        }
+        else {
+            if (formData.ServiceTitle.length > 80) {
+                setValid(true);
+                return;
+            } else {
+                console.log(formData);
+                alert(formData.Description);
+                setLoading(true);
+                setTimeout(() => {
+                    setLoading(false);
+                    setShowInput(false);
+                    setShowDiscrestion(true);
+                }, 1000);
+
+            }
+        }
+    }
+    const PostData = () => {
+        setFormData({
+            ...formData,
+            Description: DescriptionData,
+        });
+        console.log(formData);
 
     }
 
@@ -77,7 +127,7 @@ const AddNewService = () => {
         <div>
             {
                 showInput &&
-                <div className='Main-Input-From' style={{ textAlign: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <div className='Main-Input-From'>
                     <h2 style={{ color: 'rgba(98, 100, 106, 1)' }}>Please Fill This Form To Become a Service Provider</h2>
                     <div style={{ width: '70%' }}>
                         <div className='Form-Input'>
@@ -89,7 +139,7 @@ const AddNewService = () => {
                                         color="success"
                                         id="filled-error-helper-text"
                                         label="Service Title"
-                                        helperText={valid ? 'Please enter a valid number.' : ''}
+                                        helperText={valid ? 'You add Too Many Word in tittle' : ''}
                                         fullWidth
                                         required
                                         name="ServiceTitle"
@@ -103,13 +153,14 @@ const AddNewService = () => {
                                     <Select
                                         labelId="service-label"
                                         id="service-selector"
-                                        value={selectedService}
-                                        onChange={handleChange}
                                         label="Select a Home Service"
                                         fullWidth
                                         name="ServiceType"
                                         value={formData.ServiceType}
-                                        onChange={handleChangeInput}
+                                        onChange={handleChange}
+                                        helperText={valid ? 'Please select a service type.' : ''}
+                                        style={{ borderColor: valid ? 'initial' : 'red' }}
+                                        required
                                     >
                                         {services.map((service, index) => (
                                             <MenuItem key={index} value={service}>
@@ -121,11 +172,9 @@ const AddNewService = () => {
                                 <div className='Input-Field'>
                                     <label className='Lable'>Basic Price</label>
                                     <TextField
-                                        error={valid}
                                         color="success"
                                         id="filled-error-helper-text"
                                         label="Basic Price"
-                                        helperText={valid ? 'Please enter a valid number.' : ''}
                                         fullWidth
                                         required
                                         type='number'
@@ -150,6 +199,7 @@ const AddNewService = () => {
                                             </MenuItem>
                                         ))}
                                     </Select>
+
                                     <TextField
                                         id="new-task"
                                         label="Add Tags"
@@ -159,23 +209,61 @@ const AddNewService = () => {
                                         fullWidth
                                         onKeyPress={(e) => {
                                             if (e.key === 'Enter') {
+                                                e.preventDefault(); // Prevent the default behavior (form submission)
                                                 handleTaskAdd();
                                             }
                                         }}
                                         disabled={selectedTasks.length >= 5}
                                     />
                                 </div>
-                                <input type='submit' value='Submit' className='Submit-Button' onClick={SaveData} />
-                            </form  >
+                                <input
+                                    type='submit'
+                                    value='Continue'
+                                    className='Submit-Button'
+                                    onClick={SaveData}
+                                    style={{
+                                        backgroundColor: 'rgba(29, 191, 115, 1)',
+                                        color: 'white',
+                                        border: 'none',
+                                        borderRadius: '5px',
+                                        cursor: 'pointer',
+                                        float: 'right',
+                                        width: '150px',
+                                        height: '50px', 
+                                        padding: '10px 20px', 
+                                        fontSize: '20px', // Adjust the font size as needed
+                                        
+                                    }}
+                                />
+                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                    {loading && <ReactLoading type="spin" color="rgba(29, 191, 115, 1)" height={50} width={50}
+                                    />}
+                                </div>
+                            </form >
                         </div>
                     </div>
                 </div>
             }
-            {showDiscrestion && <Discrestion />}
+            {
+                showDiscrestion &&
+                <div className='Main-Input-From'>
+                     <h2 style={{ color: 'rgba(98, 100, 106, 1)' }}>Write The Complte Explanation Of Your Project </h2>
+                    <Discrestion toggleDiscrestion={UpDateShow} />  
+                </div>
+                
+            } 
+            {
+                Gallary_ &&
+                <div className='Main-Input-From'>
+                    <h2 style={{ color: 'rgba(98, 100, 106, 1)' }}>Add Some Pictures Of Your Project </h2>
+                    <Gallary />
+                </div> 
+            }
+            <button onClick={PostData} > PostData</button>
         </div>
 
 
     )
 }
 
-export default AddNewService  
+export default AddNewService   
--- se-project/src/Component/Saller/Discrestion.js
+++ se-project/src/Component/Saller/Discrestion.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Loading from "react-loading";
 
-const Discrestion = () => {
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+  ],
+};
+
+let DescriptionData = '';
+
+
+const App = ({ toggleDiscrestion }) => {
+  const [value, setValue] = useState("");
+  const [isMessageSent, setMessageSent] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [ErrorContent, setErrorContent] = useState('');
+  const handleCloseSnackbar = () => {
+    setMessageSent(false);
+  }
+
+  const SaveData = () => {
+    DescriptionData = value;
+    if (value === '') {
+      setMessageSent(true);
+      setErrorContent('Please Enter the Description')
+    }
+    else {
+      if (DescriptionData.length > 10000) {
+        setMessageSent(true);
+        console.log(DescriptionData);
+        setErrorContent('Please Enter the Description less than 1400 characters');
+        return;
+      }  
+      else {
+        setLoading(true);
+        setMessageSent(false);
+        console.log(DescriptionData);
+        DescriptionData = value;
+        setTimeout(() => {
+          setLoading(false);
+          toggleDiscrestion();
+        }, 1000);
+      }
+    }
+  }
   return (
-    <div>Discrestion</div>
-  )
-}
+    <>
+      <ReactQuill modules={modules} theme="snow" onChange={setValue} placeholder="The content starts here..." />;
+      <Snackbar open={isMessageSent} autoHideDuration={4000} onClose={handleCloseSnackbar} anchorOrigin={{ vertical: 'center', horizontal: 'center' }} >
+        <MuiAlert elevation={6} variant="filled" severity="error" onClose={handleCloseSnackbar}>
+          {ErrorContent}
+        </MuiAlert>
+      </Snackbar> 
+      <div style={{ marginLeft: '500px' }}>
+        <input
+          type='submit'
+          value='Save'
+          onClick={SaveData}
+          style={{
+            backgroundColor: 'rgba(29, 191, 115, 1)',
+            color: 'white',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            float: 'right',
+            width: '150px',
+            height: '50px',
+            padding: '10px 20px',
+            fontSize: '20px', // Adjust the font size as needed
+          }}
+        />
+      </div>
+      {isLoading && <Loading type="spin" color="rgba(29, 191, 115, 1)" height={50} width={50}
+      />}
 
-export default Discrestion
+    </>
+  );
+};
+
+
+export { DescriptionData };
+export default App;
+
