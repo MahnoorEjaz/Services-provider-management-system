64093a90cf83c83593b53b4256845747c1236f09
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-11-11 13:24:04 +0500

    program to  wargya

--- se-project/Server/Controller/ServiceController.js
+++ se-project/Server/Controller/ServiceController.js
@@ -4,22 +4,22 @@ const Service = require('../Models/AddNewService');
 
 // making the get api
 async function GetAllServices(req, res) {
-    try {
-        if (!req.user || !req.user.id) {
-            return res.status(401).json({ message: 'User not authenticated' }); 
-        }
-        const userId = req.user.id;
-        const user = await MyUser.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        const data = await Service.find({ createdBy: userId });
-        console.log(data.map((service) => service.Gallary.map((image) => image.data)));
-        res.status(200).json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: error.message });
+  try {
+    if (!req.user || !req.user.id) {
+      return res.status(401).json({ message: 'User not authenticated' });
     }
+    const userId = req.user.id;
+    const user = await MyUser.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const data = await Service.find({ createdBy: userId });
+    console.log(data.map((service) => service.Gallary.map((image) => image.data)));
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 }
 
 
@@ -30,69 +30,72 @@ async function GetAllServices(req, res) {
 // Delete Api to Delete the Data from the Database 
 async function DeleteFirst15Services(req, res) {
   try {
-      const userId = "65492d7645f3221c1b30ada4";
-      // Find the first two documents with the specified createdBy user ID
-      const documentsToDelete = await Service.find({ createdBy: userId }).limit(2);
-      if (documentsToDelete.length === 0) {
-          return res.status(404).json({ message: 'No matching documents found' });
-      }
-      // Delete the found documents
-      const deletionResult = await Service.deleteMany({ _id: { $in: documentsToDelete.map(doc => doc._id) }});
-      console.log(deletionResult);
-      res.status(200).json({ message: 'Deleted Successfully' });
+    const userId = req.user.id;
+    if (!userId) {
+      console.log('User not authenticated');
+      return res.status(401).json({ message: 'User not authenticated' });
+    }
+    const serviceId = req.params.id;
+    const service = await Service.deleteOne({ createdBy: userId, _id: serviceId });
+    if (!service) {
+      console.log('Service not found');
+      return res.status(404).json({ message: 'Service not found' });
+    }
+    res.status(200).json({ message: 'Deleted Successfully', data: service });
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    console.error('Error during service deletion:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 }
-
+ 
 // Update Api to Update the Data in the Database
 async function UpdateService(Req, Res) {
-    try {
-        const Result = await Service.findByIdAndUpdate(Req.params.id, Req.body, { new: true });
-        Res.status(201).json(Result);
-    } catch (error) {
-        Res.status(500).json({ error: error.mesage })
-    }
+  try {
+    const Result = await Service.findByIdAndUpdate(Req.params.id, Req.body, { new: true });
+    Res.status(201).json(Result);
+  } catch (error) {
+    Res.status(500).json({ error: error.mesage })
+  }
 }
 
 
 // Create a new service without images and with user reference
-async function PostProjetService(req, res) { 
-    try { 
-      const { ServiceTitle, Name, BasicPrice, Tags, ServiceType, Description, Gallary } = req.body;
-      // Extract user ID from the token payload (assuming it's already implemented)
-      const userId = req.user.id;
-      const user = await MyUser.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }     
-      // Decode the base64 image data and create an array of image objects
-      const imageObjects = Gallary.map((base64Image) => {
-        const data = base64Image
-        return {
-          data,
-        };
-      });
-      // Create a new Service instance with the image data
-      const newService = new Service({
-        ServiceTitle,
-        Name,
-        BasicPrice,
-        Tags,
-        ServiceType,
-        Description,
-        Gallary: imageObjects, // Save an array of image objects in the Gallary field
-        createdBy: user._id,
-      });
-      const result = await newService.save();
-      res.status(201).json({ result, message: 'Service created successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error creating service', error });
+async function PostProjetService(req, res) {
+  try {
+    const { ServiceTitle, Name, BasicPrice, Tags, ServiceType, Description, Gallary } = req.body;
+    // Extract user ID from the token payload (assuming it's already implemented)
+    const userId = req.user.id;
+    const user = await MyUser.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
+    // Decode the base64 image data and create an array of image objects
+    const imageObjects = Gallary.map((base64Image) => {
+      const data = base64Image
+      return {
+        data,
+      };
+    });
+    // Create a new Service instance with the image data
+    const newService = new Service({
+      ServiceTitle,
+      Name,
+      BasicPrice,
+      Tags,
+      ServiceType,
+      Description,
+      Gallary: imageObjects, // Save an array of image objects in the Gallary field
+      createdBy: user._id,
+    });
+    const result = await newService.save();
+    res.status(201).json({ result, message: 'Service created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating service', error });
   }
-  
-  
+}
+
+
 
 
 
@@ -100,10 +103,10 @@ async function PostProjetService(req, res) {
 
 // Export all the functions 
 module.exports = {
-    GetAllServices,
-    DeleteFirst15Services,
-    UpdateService,
-    PostProjetService,
+  GetAllServices,
+  DeleteFirst15Services,
+  UpdateService,
+  PostProjetService,
 
 };
 
--- se-project/src/Component/Saller/SallerViewCard.js
+++ se-project/src/Component/Saller/SallerViewCard.js
@@ -3,10 +3,20 @@ import CardContent from '@mui/joy/CardContent';
 import { CardMedia, Divider, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Link } from 'react-router-dom';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
 
 export default function BasicCard({ CardData }) {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [openDialog, setOpenDialog] = React.useState(false);
+    const [isMessageSent, setMessageSent] = React.useState(false);
+    const [ErrorContent, setErrorContent] = React.useState('');
+    const [severity, setSeverity] = React.useState('error');
+
+    const handleCloseSnackbar = () => {
+        setMessageSent(false);
+    }
     const handleMenuClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -26,52 +36,78 @@ export default function BasicCard({ CardData }) {
 
         setOpenDialog(false);
     };
-    const handleConfirmDelete = () => {
-        // Implement delete logic here
-        const token = localStorage.getItem('token');
-        const apiUrl = 'http://localhost:5000/api/DeleteService'; // Replace 'services' with your actual endpoint to retrieve services
-        const Id = CardData._id;
-        const response = fetch(apiUrl, {
-            
-        });
+    const handleConfirmDelete = async () => {
+        
+        try {
+            const id = CardData._id;
+            const token = localStorage.getItem('token');
+            const apiUrl = `http://localhost:5000/api/DeleteService/${id}`;
+            const response = await fetch(apiUrl, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token,
+                },
+            });
 
-        // Close the dialog after deletion
-        setOpenDialog(false);
+            if (response.ok) {
+                console.log('Service deleted successfully');
+                alert('Deleted Successfully');
+            } else {
+                console.error('Failed to delete service');
+                alert('Failed to delete service');
+            }
+        } catch (error) {
+            console.error('An error occurred during the delete request:', error);
+            alert('Failed to delete service', error.message);
+        } finally {
+            setOpenDialog(false);
+        }
     };
+
+
+
     return (
-        <div className='Saller-Card' style={{ maxWidth: '300px', height: '300px', cursor: 'pointer', overflow: 'hidden', borderBottom: '1px solid rgba(34, 35, 37, 1)', margin: '5px', position: 'relative' }}>
-            <CardMedia>
-                <div>
-                    <img width={300} height={200} src={CardData.Gallary[0].data} alt="CardImage" />
+        <>
+            <div className='Saller-Card' style={{ maxWidth: '300px', height: '300px', cursor: 'pointer', overflow: 'hidden', borderBottom: '1px solid rgba(34, 35, 37, 1)', margin: '5px', position: 'relative' }}>
+                <CardMedia>
+                    <div>
+                        <img width={300} height={200} src={CardData.Gallary[0].data} alt="CardImage" />
+                    </div>
+                </CardMedia>
+                <CardContent sx={{ border: '1px solid rgba(34, 35, 37, 1)', borderTop: '1px solid white', height: '150%' }}>
+                    <Link className='service-tittel' style={{ marginTop: '10px' }}>
+                        {CardData.ServiceTitle}
+                    </Link>
+                    <Divider />
+                    <div style={{ display: 'flex', justifyContent: 'space-between', maxHeight: '150%' }}>
+                        <p style={{ color: 'rgba(116, 118, 126, 1)' }}>Started At</p>
+                        <p>{CardData.BasicPrice}$</p>
+                    </div>
+                </CardContent>
+                <div style={{ position: 'absolute', top: 0, right: 0, padding: '8px', cursor: 'pointer' }}>
+                    <MoreVertIcon onClick={handleMenuClick} style={{ fontSize: 30 }} />
+                    <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                        <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+                    </Menu>
                 </div>
-            </CardMedia>
-            <CardContent sx={{ border: '1px solid rgba(34, 35, 37, 1)', borderTop: '1px solid white', height: '150%' }}>
-                <Link className='service-tittel' style={{ marginTop: '10px' }}>
-                    {CardData.ServiceTitle}
-                </Link>
-                <Divider />
-                <div style={{ display: 'flex', justifyContent: 'space-between', maxHeight: '150%' }}>
-                    <p style={{ color: 'rgba(116, 118, 126, 1)' }}>Started At</p>
-                    <p>{CardData.BasicPrice}$</p>
-                </div>
-            </CardContent>
-            <div style={{ position: 'absolute', top: 0, right: 0, padding: '8px', cursor: 'pointer' }}>
-                <MoreVertIcon onClick={handleMenuClick} style={{ fontSize: 30 }} />
-                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-                    <MenuItem onClick={handleEdit}>Edit</MenuItem>
-                    <MenuItem onClick={handleDelete}>Delete</MenuItem>
-                </Menu>
+                <Dialog open={openDialog} onClose={handleCloseDialog}>
+                    <DialogTitle>Delete Confirmation</DialogTitle>
+                    <DialogContent>
+                        Are you sure you want to delete the service?
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDialog}>Cancel</Button>
+                        <Button onClick={handleConfirmDelete} variant="contained" color="error">Delete</Button>
+                    </DialogActions>
+                </Dialog>
             </div>
-            <Dialog open={openDialog} onClose={handleCloseDialog}>
-                <DialogTitle>Delete Confirmation</DialogTitle>
-                <DialogContent>
-                    Are you sure you want to delete the service?
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseDialog}>Cancel</Button>
-                    <Button onClick={handleConfirmDelete} variant="contained" color="error">Delete</Button>
-                </DialogActions>
-            </Dialog>
-        </div>
+            <Snackbar open={isMessageSent} autoHideDuration={4000} onClose={handleCloseSnackbar} anchorOrigin={{ vertical: 'center', horizontal: 'center' }} >
+                <MuiAlert elevation={6} variant="filled" severity={severity} onClose={handleCloseSnackbar}>
+                    {ErrorContent}
+                </MuiAlert>
+            </Snackbar>
+        </>
     );
 }
